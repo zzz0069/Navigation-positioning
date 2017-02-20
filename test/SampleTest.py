697d57528a78e753eba130a492d7e6f4b13faed2
zzz0069 <zzz0069@auburn.edu>
2017-02-20 17:34:24 -0600

    Green Light |

--- test/SampleTest.py
+++ test/SampleTest.py
@@ -242,5 +242,5 @@ class SampleTest(unittest.TestCase):
             return u ** 6
         mySample = SM.Sample(self.nominalN)
         self.assertAlmostEquals(mySample.integrate(0.0, 1.0, 1, testFunction1), 0.3333, 4)
-        self.assertAlmostEquals(mySample.integrate(0.0, 1.0, 1, testFunction2), 0.1428, 4)
+        self.assertAlmostEquals(mySample.integrate(0.0, 1.0, 1, testFunction2), 0.14285, 4)
         
