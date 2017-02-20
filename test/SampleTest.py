14334342076e5c6db307562ac2567dab540ef802
zzz0069 <zzz0069@auburn.edu>
2017-02-20 17:33:52 -0600

    Green Light |

--- test/SampleTest.py
+++ test/SampleTest.py
@@ -234,4 +234,13 @@ class SampleTest(unittest.TestCase):
     def test400_020_ShouldCalculateF(self):
         mySample = SM.Sample(self.nominalN)
         self.assertAlmostEquals(mySample.f(1, 5), 0.578703704)
+
+    def test500_010_ShouldCalculateIntegrate(self):
+        def testFunction1(u,n):
+            return u ** 2
+        def testFunction2(u,n):
+            return u ** 6
+        mySample = SM.Sample(self.nominalN)
+        self.assertAlmostEquals(mySample.integrate(1.0, 1, testFunction1), 0.3333, 4)
+        self.assertAlmostEquals(mySample.integrate(1.0, 1, testFunction2), 0.1428, 4)
         
