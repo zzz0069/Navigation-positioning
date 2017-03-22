97516ebe615016af2d0f2cd7062dce11db3262fa
zzz0069 <zzz0069@auburn.edu>
2017-03-22 14:49:19 -0500

    Green Light |

--- test/NaviTest.py
+++ test/NaviTest.py
@@ -14,22 +14,22 @@ class NavigationTest(TestCase):
         self.assertDictEqual(dp.adjust(input), expected)
 
     def test100_030_ShouldReturnAltitude(self):
-        input = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-        expected = {'altitude':'45d11.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        self.assertDictEqual(dp.adjust(input), expected)
-
-    def test100_040_ShouldReturnAltitude(self):
         input = {'observation': '42d0.0',  'op': 'adjust'}
         expected = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust'}
         self.assertDictEqual(dp.adjust(input), expected)
 
-    def test100_050_ShouldReturnAltitude(self):
+    def test100_040_ShouldReturnAltitude(self):
         input = {'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
         expected = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
         self.assertDictEqual(dp.adjust(input), expected)
 
-    def test100_910_ShouldReturnNoOpError(self):
+    def test100_910_ShouldReturnError(self):
         input = {}
+        output = {'error': 'no op is specified'}
+        self.assertDictEqual(dp.dispatch(input), output)
+
+    def test100_920_ShouldReturnError(self):
+        input = {'observation': '15d04.9', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'temperature': '72'}
         output = {'error': 'no op is specified'}
         self.assertDictEqual(dp.dispatch(input), output)
 
