610d02d121590e669ef094039689b1c6068f6de3
zzz0069 <zzz0069@auburn.edu>
2017-03-22 00:00:05 -0500

    Green Light |

--- test/NaviTest.py
+++ test/NaviTest.py
@@ -2,87 +2,27 @@ from unittest import TestCase
 from softwareprocess import dispatch as dp
 
 class NavigationTest(TestCase):
-    # -----------------------------------------------------------------------
-    # ---- Acceptance Tests
-    # 100 dispatch
-    #    Analysis
-    #        inputs:
-    #           values ->    dictionary, must contain an 'op' key with values 'adjust',
-    #                       'predict', 'correct' or 'locate', mandatory, unvalidated
-    #        outputs:    dictionary of input in addition to a new altitude element
-    #    Happy path analysis:
-    #       dispatch({
-    #           'observation': '30d1.5',
-    #           'height': '19.0',
-    #           'pressure': '1000',
-    #           'horizon': 'artificial',
-    #           'op': 'adjust',
-    #           'temperature': '85'
-    #       }) -> {
-    #           'altitude': '29d59.9',
-    #           'observation': '30d1.5',
-    #           'height': '19.0',
-    #           'pressure': '1000',
-    #           'horizon': 'artificial',
-    #           'op': 'adjust',
-    #           'temperature': '85'
-    #       }
-    #    Sad path analysis:
-    #       dispatch({'op': 'locate'}) -> {'error': 'no op is specified'}
-    #       dispatch({
-    #           'observation': '15d04.9',
-    #           'height': '6.0',
-    #           'pressure': '1010',
-    #           'horizon': 'artificial',
-    #           'temperature': '72'
-    #       }) -> {
-    #           'observation': '15d04.9',
-    #           'height': '6.0',
-    #           'pressure': '1010',
-    #           'horizon': 'artificial',
-    #           'temperature': '72',
-    #           'error':'no op is specified'
-    #       }
-    #       dispatch(42) -> {'error':'parameter is not a dictionary'}
-    #       dispatch({'op': 'unknown'}) -> {'error':'op is not a legal operation'}
-    #       dispatch() -> {'error':'dictionary is missing'}
-    # Happy path
+
     def test100_010_ShouldCalculateAltitude(self):
-        input = { 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        output = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        input = { 'observation': '15d04.9', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'temperature': '72'}
+        output = { 'error': 'no op is specified', 'observation': '15d04.9', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'temperature': '72'}
         self.assertDictEqual(dp.dispatch(input), output)
-    # Sad path
+
     def test100_910_ShouldReturnNoOpError(self):
         input = {}
         output = {'error': 'no op is specified'}
         self.assertDictEqual(dp.dispatch(input), output)
 
-    def test100_920_ShouldReturnNoOpError(self):
-        input = {
-              'observation': '15d04.9',
-              'height': '6.0',
-              'pressure': '1010',
-              'horizon': 'artificial',
-              'temperature': '72'
-          }
-        output = {
-              'error': 'no op is specified',
-              'observation': '15d04.9',
-              'height': '6.0',
-              'pressure': '1010',
-              'horizon': 'artificial',
-              'temperature': '72'
-          }
-        self.assertDictEqual(dp.dispatch(input), output)
-
-    #       dispatch(42) -> {'error':'parameter is not a dictionary'}
     def test100_930_ShouldReturnErrorIfInputIsNotDict(self):
         input = 42
         output = {'error':'parameter is not a dictionary'}
         self.assertDictEqual(dp.dispatch(input), output)
 
-    #       dispatch({'op': 'unknown'}) -> {'error':'op is not a legal operation'}
-    # NOTE: Going with umphress's implementation that combines previous key-values Not excel
+    def test100_920_ShouldReturnNoOpError(self):
+        input = { 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        output = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        self.assertDictEqual(dp.dispatch(input), output)
+
     def test100_940_ShouldReturnIllegalOpError(self):
         input = {'op': 'unknown'}
         output = {
@@ -91,38 +31,25 @@ class NavigationTest(TestCase):
         }
         self.assertDictEqual(dp.dispatch(input), output)
 
-    #       dispatch() -> {'error':'dictionary is missing'}
-    # again, inconsistent with umphress's implementation and excel's. going with umphress again
     def test100_950_ShouldReturnDictMissingError(self):
         output = {'error':'parameter is missing'}
         self.assertDictEqual(dp.dispatch(), output)
 
-#---- Unit tests
-#
-# 200 adjust
-#     Analysis
-#        inputs:
-#            values ->  dict mandatory validated (private functions are usually validated)
-#     Happy path:
-#            adjust(validDict) -> dictionary with a new altitude element calculated
-#     Sad path:
-#            adjust(invalidDict) -> dictionary with an error corresponding to the invalid element
-#
-    # Happy path
     def test200_010_ShouldReturnCalcuatedAltitude(self):
         input = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         expected = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         self.assertDictEqual(dp.adjust(input), expected)
+
     def test200_020_ShouldReturnCalcuatedAltitude(self):
         input = {'observation': '42d0.0',  'op': 'adjust'}
         expected = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust'}
         self.assertDictEqual(dp.adjust(input), expected)
+
     def test200_030_ShouldReturnCalcuatedAltitude(self):
         input = {'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
         expected = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
         self.assertDictEqual(dp.adjust(input), expected)
 
-    # Sad path
     def test200_910_ShouldReturnMandatoryElemMissingError(self):
         input = {'op': 'adjust'}
         expected = {'op': 'adjust', 'error':'mandatory information is missing'}
@@ -148,27 +75,12 @@ class NavigationTest(TestCase):
         expected = {'observation': '45d15.2', 'height': '6', 'horizon': 2, 'pressure': '1010', 'op': 'adjust', 'temperature': '71', 'error':'horizon is invalid'}
         self.assertDictEqual(dp.adjust(input), expected)
 
-    # TODO: test if the elements aren't correct types using try/except
-
-
-
-
-
-
-
     def shouldReturnErrorIfAltitudeExists(self):
-        input = {
-            'altitude': 'something'
-        }
-        expected = {
-            'altitude': 'something',
-            'error': 'altitude already exists in the input'
-        }
+        input = {'altitude': 'something'}
+        expected = {'altitude': 'something', 'error': 'altitude already exists in the input'}
         actual = dp.adjust(input)
         self.assertDictEqual(expected, actual)
 
     def shouldReturnErrorIfObservationDoesNotExist(self):
-        input = {
-            ''
-        }
+        input = { ''}
 
