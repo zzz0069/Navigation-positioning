44046cad645ea24cbe13a3216a9dd41b4efd81ce
zzz0069 <zzz0069@auburn.edu>
2017-03-21 21:03:34 -0500

    Green Light |

--- test/NaviTest.py
+++ test/NaviTest.py
@@ -1,5 +1,4 @@
-from unittest import TestCase
-import dispatch as nav
+import dispatch as dispatch
 
 class NavigationTest(TestCase):
     # -----------------------------------------------------------------------
@@ -65,12 +64,12 @@ class NavigationTest(TestCase):
             'op': 'adjust',
             'temperature': '85'
         }
-        self.assertDictEqual(nav.dispatch(input), output)
+        self.assertDictEqual(dispatch.dispatch(input), output)
     # Sad path
     def test100_910_ShouldReturnNoOpError(self):
         input = {}
         output = {'error': 'no op is specified'}
-        self.assertDictEqual(nav.dispatch(input), output)
+        self.assertDictEqual(dispatch.dispatch(input), output)
 
     def test100_920_ShouldReturnNoOpError(self):
         input = {
@@ -88,13 +87,13 @@ class NavigationTest(TestCase):
               'horizon': 'artificial',
               'temperature': '72'
           }
-        self.assertDictEqual(nav.dispatch(input), output)
+        self.assertDictEqual(dispatch.dispatch(input), output)
 
     #       dispatch(42) -> {'error':'parameter is not a dictionary'}
     def test100_930_ShouldReturnErrorIfInputIsNotDict(self):
         input = 42
         output = {'error':'parameter is not a dictionary'}
-        self.assertDictEqual(nav.dispatch(input), output)
+        self.assertDictEqual(dispatch.dispatch(input), output)
 
     #       dispatch({'op': 'unknown'}) -> {'error':'op is not a legal operation'}
     # NOTE: Going with umphress's implementation that combines previous key-values Not excel
@@ -104,13 +103,13 @@ class NavigationTest(TestCase):
             'op': 'unknown',
             'error':'op is not a legal operation'
         }
-        self.assertDictEqual(nav.dispatch(input), output)
+        self.assertDictEqual(dispatch.dispatch(input), output)
 
     #       dispatch() -> {'error':'dictionary is missing'}
     # again, inconsistent with umphress's implementation and excel's. going with umphress again
     def test100_950_ShouldReturnDictMissingError(self):
         output = {'error':'parameter is missing'}
-        self.assertDictEqual(nav.dispatch(), output)
+        self.assertDictEqual(dispatch.dispatch(), output)
 
 #---- Unit tests
 #
@@ -127,41 +126,41 @@ class NavigationTest(TestCase):
     def test200_010_ShouldReturnCalcuatedAltitude(self):
         input = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         expected = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        self.assertDictEqual(nav.adjust(input), expected)
+        self.assertDictEqual(dispatch.adjust(input), expected)
     def test200_020_ShouldReturnCalcuatedAltitude(self):
         input = {'observation': '42d0.0',  'op': 'adjust'}
         expected = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust'}
-        self.assertDictEqual(nav.adjust(input), expected)
+        self.assertDictEqual(dispatch.adjust(input), expected)
     def test200_030_ShouldReturnCalcuatedAltitude(self):
         input = {'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
         expected = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
-        self.assertDictEqual(nav.adjust(input), expected)
+        self.assertDictEqual(dispatch.adjust(input), expected)
 
     # Sad path
     def test200_910_ShouldReturnMandatoryElemMissingError(self):
         input = {'op': 'adjust'}
         expected = {'op': 'adjust', 'error':'mandatory information is missing'}
-        self.assertDictEqual(nav.adjust(input), expected)
+        self.assertDictEqual(dispatch.adjust(input), expected)
 
     def test200_920_ShouldReturnInvalidObservationError(self):
         input = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         expected = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error':'observation is invalid'}
-        self.assertDictEqual(nav.adjust(input), expected)
+        self.assertDictEqual(dispatch.adjust(input), expected)
 
     def test200_910_ShouldReturnInvalidHeightError(self):
         input = {'observation': '45d15.2', 'height': 'a', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         expected = {'observation': '45d15.2', 'height': 'a', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error':'height is invalid'}
-        self.assertDictEqual(nav.adjust(input), expected)
+        self.assertDictEqual(dispatch.adjust(input), expected)
 
     def test200_920_ShouldReturnInvalidHorizonError(self):
         input = {'observation': '45d15.2', 'height': '6', 'horizon': '   ', 'pressure': '1010', 'op': 'adjust', 'temperature': '71'}
         expected = {'observation': '45d15.2', 'height': '6', 'horizon': '   ', 'pressure': '1010', 'op': 'adjust', 'temperature': '71', 'error':'horizon is invalid'}
-        self.assertDictEqual(nav.adjust(input), expected)
+        self.assertDictEqual(dispatch.adjust(input), expected)
 
     def test200_930_ShouldReturnInvalidHorizonError(self):
         input = {'observation': '45d15.2', 'height': '6', 'horizon': 2, 'pressure': '1010', 'op': 'adjust', 'temperature': '71'}
         expected = {'observation': '45d15.2', 'height': '6', 'horizon': 2, 'pressure': '1010', 'op': 'adjust', 'temperature': '71', 'error':'horizon is invalid'}
-        self.assertDictEqual(nav.adjust(input), expected)
+        self.assertDictEqual(dispatch.adjust(input), expected)
 
     # TODO: test if the elements aren't correct types using try/except
 
@@ -179,7 +178,7 @@ class NavigationTest(TestCase):
             'altitude': 'something',
             'error': 'altitude already exists in the input'
         }
-        actual = nav.adjust(input)
+        actual = dispatch.adjust(input)
         self.assertDictEqual(expected, actual)
 
     def shouldReturnErrorIfObservationDoesNotExist(self):
