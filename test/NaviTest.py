60807870c05832426f21842c47288bd950f1343a
zzz0069 <zzz0069@auburn.edu>
2017-03-22 14:44:28 -0500

    Green Light |

--- test/NaviTest.py
+++ test/NaviTest.py
@@ -3,10 +3,30 @@ from softwareprocess import dispatch as dp
 
 class NavigationTest(TestCase):
 
-    def test100_010_ShouldCalculateAltitude(self):
+    def test100_010_ShouldReturnAltitude(self):
         input = { 'observation': '15d04.9', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'temperature': '72'}
         output = {'observation': '15d04.9', 'height': '6.0', 'pressure': '1010', 'horizon': 'artificial', 'temperature': '72', 'error':'no op is specified'}
         self.assertDictEqual(dp.dispatch(input), output)
+
+    def test100_020_ShouldReturnAltitude(self):
+        input = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        expected = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        self.assertDictEqual(dp.adjust(input), expected)
+
+    def test100_030_ShouldReturnAltitude(self):
+        input = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        expected = {'altitude':'45d11.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        self.assertDictEqual(dp.adjust(input), expected)
+
+    def test100_040_ShouldReturnAltitude(self):
+        input = {'observation': '42d0.0',  'op': 'adjust'}
+        expected = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust'}
+        self.assertDictEqual(dp.adjust(input), expected)
+
+    def test100_050_ShouldReturnAltitude(self):
+        input = {'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
+        expected = {'altitude':'45d11.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        self.assertDictEqual(dp.adjust(input), expected)
 
     def test100_910_ShouldReturnNoOpError(self):
         input = {}
@@ -26,11 +46,6 @@ class NavigationTest(TestCase):
     def test100_950_ShouldReturnDictMissingError(self):
         output = {'error':'parameter is missing'}
         self.assertDictEqual(dp.dispatch(), output)
-
-    def test200_010_ShouldReturnCalcuatedAltitude(self):
-        input = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        expected = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        self.assertDictEqual(dp.adjust(input), expected)
 
     def test200_020_ShouldReturnCalcuatedAltitude(self):
         input = {'observation': '42d0.0',  'op': 'adjust'}
