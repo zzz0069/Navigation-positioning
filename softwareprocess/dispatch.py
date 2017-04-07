baa2081fdb4c61de745bdeb831841b299dba1ed9
zzz0069 <zzz0069@auburn.edu>
2017-04-07 15:46:31 -0500

    Green Light |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -133,7 +133,7 @@ def predict(values):
         return values
 
 
-    data = open('Stars.txt').read()
+    data = open('Stars.txt')
     starsDict = {}
     for line in data:
         eachLine = line.split()
