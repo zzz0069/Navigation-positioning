cee74adddd9ff17e59351e47f81a363f58ef2336
zzz0069 <zzz0069@auburn.edu>
2017-04-07 15:31:06 -0500

    Green Light |

--- softwareprocess/main dispatch.py
+++ softwareprocess/main dispatch.py
@@ -1,5 +1,5 @@
 
 from softwareprocess import dispatch as dp
 
-a = dp.dispatch({'op':'predict', 'body': 'unknown', 'date': '2016-01-17', 'time': '03:15:42'})
+a = dp.dispatch( {'op':'predict', 'body': 'Betelgeuse', 'date': '2016-01-17', 'time': '03:15:42'})
 print a
