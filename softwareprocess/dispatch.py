14af267d9b59696645685e6a30d07a9a4d4345f2
zzz0069 <zzz0069@auburn.edu>
2017-04-07 01:50:38 -0500

    Green Light |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -165,38 +165,60 @@ def predict(values):
     if key not in dict.keys(values):
         values[key] = '2001-01-01'
     else:
-        flag = dateTest(values['date'])
-        if flag == False:
-            values['error'] = 'date value is illegal'
+        dateValid = dateTest(values['date'])
+        if dateValid == False:
+            values['error'] = 'invalid date'
             return values
     key = 'time'
     if key not in dict.keys(values):
         values[key] = '00:00:00'
     else:
-        flag = timeTest(values)
-        if flag == False:
-            values['error'] = 'time value is illegal'
+        timeValid = timeTest(values)
+        if timeValid == False:
+            values['error'] = 'invalid time'
             return values
 
 
     starParameters = starsDict[starName]
     starParameters = starParameters.split()
-    latitude = starParameters[1]
     SHA = starParameters[0]
+    latitude = starParameters[1]
     timeParameters = {'date' : values['date'], 'time' : values['time']}
     earthGHA = calculateGHA(timeParameters)
-    long = degreeToFloat(earthGHA) + degreeToFloat(SHA)
-    long = long - (int(long / 360) * 360)
-    long = degreeToString(long)
-    values['long'] = long
+    GHAStar = degreeToFloat(earthGHA) + degreeToFloat(SHA)
+    GHAStar = GHAStar - (int(GHAStar / 360) * 360)
+    GHAStar = degreeToString(GHAStar)
+    values['long'] = GHAStar
     values['lat'] = latitude
     for key in dict.keys(values):
         if key not in keys:
             del values[key]
     return values
 
+def dateTest(value):
+    if not re.match("^\d\d\d\d-\d\d-\d\d$", value):
+        return False
+    value = value.split('-')
+    if int(value[0]) < 2001 or int(value[1]) < 1 or int(value[1]) > 12:
+        return False
+    year = int(value[0])
+    month = int(value[1])
+    date = int(value[2])
+
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        if date > 31:
+            return False
+    if month in [4, 6, 9, 11]:
+        if date > 30:
+            return False
+    if month == 2 and year % 4 != 0:
+        if date > 28:
+            return False
+    if month == 2 and year % 4 == 0:
+        if date > 29:
+            return False
+
 def timeTest(values):
-    # validate time
     time = values['time']
     if not re.match("^\d\d:\d\d:\d\d", time):
         return False
@@ -204,34 +226,8 @@ def timeTest(values):
     if (int(time[0]) > 24 or int(time[0]) < 0) or (int(time[1]) > 60 or int(time[1]) < 0) or (int(time[2]) > 60 or int(time[2]) <0):
         return False
 
-
-def dateTest(value):
-    # validate date
-    if not re.match("^\d\d\d\d-\d\d-\d\d$", value):
-        return False
-    value = value.split('-')
-    if int(value[0]) < 2001 or int(value[1]) > 12:
-        return False
-    date = int(value[2])
-    month = int(value[1])
-    year = int(value[0])
-    if month == 2 and year % 4 != 0:
-        if date > 28:
-            return False
-    if month == 2 and year % 4 == 0:
-        if date > 29:
-            return False
-    if month in [1, 3, 5, 7, 8, 10, 12]:
-        if date > 31:
-            return False
-    if month in [4, 6, 9, 11]:
-        if date > 30:
-            return False
-
-
-#calculate the GHA to the date
 def calculateGHA(timeParameters):
-    originalGHA = '100d42.6'
+    originalGHA = '100d42.6'        # GHA in 2001-01-01
     originalGHA = degreeToFloat(originalGHA)
     date = timeParameters['date']
     time = timeParameters['time']
@@ -239,19 +235,18 @@ def calculateGHA(timeParameters):
     month = int(date.split('-')[1])
     day = int(date.split('-')[2])
     yearGap = year - 2001
-    cumulativeProgress = yearGap * degreeToFloat('-0d14.31667')
+    cumulativeProgression = yearGap * degreeToFloat('-0d14.31667')
     leapYears = int(yearGap / 4)
     dailyRotation = degreeToFloat('0d59.0')
     totalProgression = dailyRotation * leapYears
-    # the rotation to the time
-    beginningOfTheYear = datetime.date(year,1,1)
+    firstDayOfTheYear = datetime.date(year,1,1)
     currentDate = datetime.date(year,month,day)
-    diff = currentDate - beginningOfTheYear
+    diff = currentDate - firstDayOfTheYear
     dayGap = int(diff.days)
     time = time.split(':')
     sec = dayGap * 86400 + int(time[0]) * 3600 + int(time[1]) * 60 + int(time[2])
     rotationInYear = (sec - int(sec / 86164.1) * 86164.1) / 86164.1 * degreeToFloat('360d0')
-    GHA = originalGHA + cumulativeProgress + totalProgression + rotationInYear
+    GHA = originalGHA + cumulativeProgression + totalProgression + rotationInYear
     GHA = degreeToString(GHA)
     return GHA
 
