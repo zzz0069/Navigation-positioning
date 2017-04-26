e4cc833327369404c74ac60e087eb3eb85432783
zzz0069 <zzz0069@auburn.edu>
2017-04-26 01:05:39 -0500

    Green Light |

--- softwareprocess/main dispatch.py
+++ softwareprocess/main dispatch.py
@@ -1,9 +1,4 @@
 
 from softwareprocess import dispatch as dp
-c=dp.degreeToFloat('0d59.0')
-c *= 3
-print c
-d=dp.degreeToString(c)
-print d
 a = dp.dispatch({'op':'predict', 'body': 'Betelgeuse', 'date': '2016-01-17', 'time': '03:15:42'})
 print a
